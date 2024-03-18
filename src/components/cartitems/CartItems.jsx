92c7d965ed19e073dd872b232ad88794a0b005da
hocinedev <hocinedev4@gmail.com>
2024-03-18 06:40:46 +0100

    update 3

--- src/components/cartitems/CartItems.jsx
+++ src/components/cartitems/CartItems.jsx
@@ -17,24 +17,54 @@ const CartItems = () => {
 <p>Quantity</p>
 <p>Total</p>
 <p>Remove</p>
+</div>
 <hr />
 {allProducts.map((item) =>{
     if(cartItems[item.id] > 0){
         return <div>
-        <div className="format">
+        <div className="format main">
             <img src={item.image} alt={item.image} className="icon" />
             <p>{item.name}</p>
             <p>${item.new_price}</p>
             <button className='qty'>{cartItems[item.id]}</button>
-            <p>{item.new_price * cartItems[item.id]} </p>
-            <img src={removeIcon} alt="remove icon" onClick={()=>{removeFromCart(item.id)}} />
+            <p>${item.new_price * cartItems[item.id]} </p>
+            <img  className="removeIcon" src={removeIcon} alt="remove icon" onClick={()=>{removeFromCart(item.id)}} />
         </div>
         <hr />
     </div>
     }else{return null}
 })}
-</div>
+<div className="down">
+    <div className="total">
+        <h1>Cart Totals</h1>
+        <div>
+            <div className="total-items">
+                <p>Sub Total</p>
+                <p>${0}</p>
+            </div>
+            <hr />
+            <div className="total-items">
+                <p>Shipping</p>
+                <p>Free</p>
+            </div>
+            <hr />
+            <div className="total-items">
+                <h3>Total</h3>
+                <h3>${0}</h3>
+            </div>
+        </div>
+        <button>Proceed To Payment</button>
     </div>
+    <div className="promo">
+        <p>If You Have A Promo Code Enter It Here</p>
+        <div className="promo-box">
+            <input type="text" placeholder='Promo Code' />
+            <button>Submit</button>
+        </div>
+    </div>
+</div>
+</div>
+    
   )
 }
 
