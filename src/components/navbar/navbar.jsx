72931febb757e2f4f765695368b4aec23dbf392d
hocinedev <hocinedev4@gmail.com>
2024-03-21 06:36:38 +0100

    nav bar finished

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,25 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './navbar.css'
 import logo from '../assets/logo.png'
 import cart from '../assets/cart.png'
+import dropDown from '../assets/hambo.png'
 import { Link } from 'react-router-dom'
 import { shopContext } from '../../context/Shopcontext'
 
 const Navbar = () => {
     const [Menu,setMenu] = useState('')
     const {GetTotalItems} = useContext(shopContext)
+    const menuRef = useRef()
+    const dropDownToggle = (e)=>{
+menuRef.current.classList.toggle('show')
+    }
   return (
     <div className='navbar'>
         <div className="nav-logo">
            <Link to='/'> <img src={logo} alt="logo" /></Link>
         </div>
-        <ul className="nav-menu">
+        <img onClick={dropDownToggle} src={dropDown} className='navDropDown' alt="nav drop down" />
+        <ul className="nav-menu"  ref={menuRef} >
             <li onClick={()=>{setMenu('shop')}}> <Link to='/'>Shop</Link> {Menu === 'shop'? <hr/>:null}</li>
             <li onClick={()=>{setMenu('men')}}> <Link to='/men'>Men</Link>  {Menu === 'men'? <hr/>:null}</li>
             <li onClick={()=>{setMenu('women')}}><Link to='/women'>Women</Link>  {Menu === 'women'? <hr/>:null}</li>
